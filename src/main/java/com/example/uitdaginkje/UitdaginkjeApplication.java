b5b2b90b6d86ac135cbd2e02059a334b73607ce0
kvanheuverbek <karl.vanheuverbeke@telenet.be>
2024-12-31 08:57:35 +0100

    initial commit

---
+++ src/main/java/com/example/uitdaginkje/UitdaginkjeApplication.java
@@ -0,0 +1,13 @@
+package com.example.uitdaginkje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UitdaginkjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UitdaginkjeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/uitdaginkje/UitdaginkjeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uitdaginkje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UitdaginkjeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
